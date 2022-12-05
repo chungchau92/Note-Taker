3cd6b0bb358c7d320bef3ae051b2de978272b270
chungchau <chungchau92@gmail.com>
2022-12-05 17:31:34 +0700

    add notes Routes

--- Develop/routers/notes.js
+++ Develop/routers/notes.js
@@ -3,5 +3,18 @@ const notes = require("express").Router();
 const fs = require('fs');
 
 notes.get("/api/notes", (req, res) => {
-    fs.readFile()
+    fs.readFile("./db/db.json" , "utf8", (err,data) => {
+        if(err) {
+            console.log(err)
+        } else {
+            res.json(JSON.parse(data))
+        }
+    }) 
 });
+
+notes.post("/api/notes", (req,res) => {
+    const { title, text } = req.body;
+    
+})
+
+module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -6,19 +6,23 @@ const PORT = process.env.port || 3001;
 
 const app = express();
 
+const notes = require("./routers/notes")
+
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.json());
 
 app.use(express.static("public"));
 
+app.use("/", notes)
+
 app.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+// app.get("*", (req,res) => {
+//     res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT} 🚀`);
